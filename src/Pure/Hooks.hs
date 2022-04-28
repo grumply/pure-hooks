054ec013ca8441e6ebceb1bf6960547e3f957738
Sean Mackenzie <sean@grump.ly>
2022-04-27 21:18:39 -0500

    Remove state; prefer Pure.Elm.Fold state.

--- src/Pure/Hooks.hs
+++ src/Pure/Hooks.hs
@@ -9,5 +9,4 @@ import Pure.Hooks.Memo as Export
 import Pure.Hooks.Producer as Export
 import Pure.Hooks.Reducer as Export
 import Pure.Hooks.Ref as Export
-import Pure.Hooks.State as Export
 import Pure.Hooks.Throttle as Export
